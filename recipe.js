96e8f845c86c4834b29945b84432410b72afb8b6
Saul Ocampo <digitalh2o@gmail.com>
2016-09-02 18:48:07 +0000

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,22 @@
 'use strict';
+
+function addIngredient(recipeHash, ingredient, quantity) {
+  recipeHash[ingredient] = quantity;
+  return recipeHash
+}
+
+function removeIngredient(recipe, ingredient) {
+  delete recipe[ingredient]
+  return recipe;
+}
+
+function updateIngredient(recipe, ingredient, quantity) {
+  recipe[ingredient] = quantity
+  return recipe;
+}
+
+function readRecipe(recipeHash) {
+  for (var key in recipeHash){
+    console.log("this recipe calls for " + recipeHash[key] + " of " + key);
+  }
+}
